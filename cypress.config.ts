076129e592c61818148c67d36fece39c93681fe9
Nora Hamida <hamida.nora@gmail.com>
2025-01-10 12:25:09 +0100

    "feat(cypress): installation de Cypress et initialisation de la configuration"

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,28 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
+import path from 'path';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const options = {
+        ...webpackPreprocessor.defaultOptions,
+        webpackOptions: {
+          ...webpackPreprocessor.defaultOptions.webpackOptions,
+          resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx'], 
+            alias: {
+              '@src': path.resolve(__dirname, 'src'),  
+            },
+          },
+        },
+      };
+
+      on('file:preprocessor', webpackPreprocessor(options));
+
+      return config;
     },
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
